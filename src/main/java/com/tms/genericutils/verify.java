1b39ddd2562cdd8521ae3609c736abc14f2c5449
shivakumarHulagur9911@gmail.com <shivakumarhulagur9911@gmail.com>
2023-10-01 10:57:38 +0530

    TMS_First_Commit

---
+++ src/main/java/com/tms/genericutils/verify.java
@@ -0,0 +1,17 @@
+package com.tms.genericutils;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+
+public class verify {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		
+		FileInputStream fis=new FileInputStream("vhvugpug");
+		
+		FileOutputStream fout=new FileOutputStream("jb");
+	}
+}
